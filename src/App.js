54d49eb903316c3712129bcaa3e8f1d3deb085ce
John Suico <johncdsuico@gmail.com>
2020-07-06 22:03:38 -0700

    create project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+
+// function App() {
+//   const [todos, setTodos] = useState([]);
+
+//   useEffect(() => {
+//     Axios.get('https://jsonplaceholder.typicode.com/todos')
+//       .then(res => {
+//         setTodos(res.data);
+//       })
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h1>React App</h1>
+//       {todos.map((todo) => (
+//         <li key={todo.id}>{todo.title}</li>
+//       ))};
+//     </div>
+//   );
+// }
 
 function App() {
+  const [movie, setMovie] = useState([]);
+  const [click, setClick] = useState(false);
+
+  useEffect(() => {
+    Axios.get("http://omdbapi.com/?apikey=5000d172&t=Milpitas")
+      .then(res => {
+        setMovie(res.data);
+        // console.log(res.data);
+        // console.log(res.data.Title);
+      })
+  }, []);
+
+  function handleClick() {
+    setClick(!click);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Title: {movie.Title}</h1>
+      <img src={movie.Poster} alt={movie.Title + ' Poster'}/>
+      <button onClick={handleClick}> Click Me! </button>
+      {click && <h1>You Clicked me</h1>}
     </div>
-  );
+  )
 }
 
 export default App;
